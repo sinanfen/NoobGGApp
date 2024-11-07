9ff7a0fba2a9a931e7cfcea283ded69bd40bd123
Sinan Fen <sinanfen@hotmail.com>
2024-11-07 22:36:12 +0300

    interface for dbcontext

---
+++ src/NoobGGApp.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NoobGGApp.Domain.Entities;
+
+namespace NoobGGApp.Application.Common.Interfaces;
+
+public interface IApplicationDbContext
+{
+    DbSet<GameRegion> GameRegions { get; set; }
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    int SaveChanges();
+}
