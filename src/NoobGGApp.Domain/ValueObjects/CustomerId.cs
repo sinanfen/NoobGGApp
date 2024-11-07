1bf23505aeeb452a2fc6ade47f4dfd3f5f0b6c5d
Sinan Fen <sinanfen@hotmail.com>
2024-11-07 22:36:43 +0300

    value object

---
+++ src/NoobGGApp.Domain/ValueObjects/CustomerId.cs
@@ -0,0 +1,12 @@
+﻿public record struct CustomerId
+{
+    public long Value { get; init; }
+
+    public CustomerId(long value)
+    {
+        if (value <= 0)
+            throw new ArgumentException("Customer ID cannot be less than or equal to 0");
+
+        Value = value;
+    }
+}
