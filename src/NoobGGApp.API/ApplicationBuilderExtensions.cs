b1915b8e2058d89422ae7c0aa0000726657dffcc
Sinan Fen <sinanfen@hotmail.com>
2024-11-28 00:16:08 +0300

    api has been updated

---
+++ src/NoobGGApp.API/ApplicationBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NoobGGApp.Infrastructure.Persistence.EntityFramework.Contexts;
+
+namespace NoobGGApp.WebApi;
+
+public static class ApplicationBuilderExtensions
+{
+    public static IApplicationBuilder ApplyMigrations(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        if (context.Database.GetPendingMigrations().Any())
+            context.Database.Migrate();
+
+        return app;
+    }
+
+}
